cb286034346085e0544230c85456098b9842259e
Rerurate514 <rerurateyuto@gmail.com>
2024-04-09 12:23:49 +0900

    タイプライターコンポーネントの作成

---
+++ src/components/typeWriter.vue
@@ -0,0 +1,25 @@
+<template>
+    <p>{{ showText }}</p>
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+    import { toRef } from 'vue'
+    const SLEEP_TIME = 90
+
+    const props = defineProps({
+        text: String,
+    })
+
+    let showText = ref("")
+    const propsText = toRef(props, "text")
+    const strLen = propsText.value.length
+
+    for(let i = 0; i < strLen; i++){
+        setTimeout(() => {
+            showText.value += propsText.value.substring(i, i + 1)
+        }, SLEEP_TIME * i);
+    }
+
+    
+</script>
