05232bcb38c0e7dd622321ea2691ed5011efe43d
Rerurate514 <rerurateyuto@gmail.com>
2024-04-28 23:19:44 +0900

    黒地コンポーネントの追加

---
+++ src/components/blackLabel.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="blacklabel">
+        <h1>{{ props.text }}</h1>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        text: String
+    })
+</script>
+
+<style>
+    .blacklabel{
+        margin: 16px;
+        background-color: #201919;
+    }
+</style>
